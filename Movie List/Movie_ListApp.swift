fc356c1e9721d3a7f24818718728f447d6115c52
macbookpro <munjur533@gmail.com>
2022-09-22 10:48:44 +0600

    Initial Commit

---
+++ Movie List/Movie_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ListApp.swift
+//  Movie List
+//
+//  Created by MacBook Pro on 22/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
